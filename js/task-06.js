7e0c2ed362e74b46aacf9d11842efcec83796a14
MakarenkoYevhenii <92297867+MakarenkoYevhenii@users.noreply.github.com>
2022-01-12 21:37:55 +0200

    make refractoring of code

--- js/task-06.js
+++ js/task-06.js
@@ -2,16 +2,12 @@ const textHolder = document.querySelector('#validation-input')
 
 textHolder.addEventListener("blur",() =>
 {
-     if(textHolder.value === `6 symbols`)
+     if(textHolder.value === `6 symbols` && textHolder.value.length === Number(textHolder.dataset.length))
     {
         textHolder.classList.add("valid")
-    }
-    else if(textHolder.value.length === Number(textHolder.dataset.length)){   
         textHolder.classList.remove("invalid")
-        textHolder.classList.add("valid")
     }
-   
-    else{textHolder.classList.add("invalid")
+    textHolder.classList.add("invalid")
     textHolder.classList.remove("valid")
-}
+
 })

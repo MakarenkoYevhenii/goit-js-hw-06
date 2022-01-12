05ec088858ab5875b1990f7ef8633bf2f3e57bc2
MakarenkoYevhenii <92297867+MakarenkoYevhenii@users.noreply.github.com>
2022-01-12 21:45:19 +0200

    fdsfsd

--- js/task-06.js
+++ js/task-06.js
@@ -2,10 +2,10 @@ const textHolder = document.querySelector('#validation-input')
 
 textHolder.addEventListener("blur",() =>
 {
-     if(textHolder.value === `6 symbols` && textHolder.value.length === Number(textHolder.dataset.length))
+     if(textHolder.value === `6 symbols` || textHolder.value.length === Number(textHolder.dataset.length))
     {
-        textHolder.classList.add("valid")
         textHolder.classList.remove("invalid")
+        textHolder.classList.add("valid")
     }
     textHolder.classList.add("invalid")
     textHolder.classList.remove("valid")
